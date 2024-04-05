d1e2697076080dd1d7aa031ec8324d176c4b3784
AbdulRahmanPro <progammerabood@gmail.com>
2024-04-05 21:10:41 +0300

    Adding animation to the Nav Bar

--- components/Navbar/index.jsx
+++ components/Navbar/index.jsx
@@ -1,33 +1,52 @@
-import React from 'react';
+"use client"
+import { useEffect, useState } from 'react';
 import Logo from '@/assets/Icons/Logo';
 import ArrowYellow from '@/assets/Icons/ArrowYellow';
-import './index.styles.scss'; // Assuming you have a regular CSS file for styles
+import './index.styles.scss';
 import { navLinks } from './_constants';
 
-const Navbar = ({ type }) => (
-  <div className={type === 'dark' ? 'bgDark' : 'bgLight navWrapper'}>
-    <nav className="nav">
-      <div>
-        <Logo />
-      </div>
-      <div className="nav-links">
-        {navLinks.map((item) => (
-          <p
-            style={{ color: type === 'dark' ? 'white' : 'black' }}
-            key={item.title}
-          >
-            {item.title}
-          </p>
-        ))}
-      </div>
-      <div className="buttonWrapper">
-        <button type="button" className="button">
-          Contact us
-        </button>
-        <ArrowYellow />
-      </div>
-    </nav>
-  </div>
-);
+const Navbar = ({ type }) => {
+  const [scrollY, setScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const newPosition = window.scrollY;
+      console.log(newPosition); // الآن سيتم طباعة الموقع الجديد للسكرول
+      setScrollY(newPosition);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <div className={`${type === 'dark' ? 'bgDark' : 'bgLight'} navWrapper ${ scrollY > 0 ? 'navEffect' : ''}`}>
+  <nav className="nav">
+        <div>
+          <Logo />
+        </div>
+        <div className="nav-links">
+          {navLinks.map((item) => (
+            <p
+              style={{ color: type === 'dark' ? 'white' : 'black' }}
+              key={item.title}
+            >
+              {item.title}
+            </p>
+          ))}
+        </div>
+        <div className="buttonWrapper">
+          <button type="button" className="button">
+            Contact us
+          </button>
+          <ArrowYellow />
+        </div>
+      </nav>
+    </div>
+  );
+};
 
 export default Navbar;
