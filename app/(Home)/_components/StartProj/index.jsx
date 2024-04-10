e6995acd72353f89085fe98174340b994109735f
AbdulRahmanPro <progammerabood@gmail.com>
2024-04-10 23:39:15 +0300

    Finish making your home page and making it responsive

--- app/(Home)/_components/StartProj/index.jsx
+++ app/(Home)/_components/StartProj/index.jsx
@@ -5,17 +5,33 @@ import '@/styles/index.scss';
 import Button from '../Button/index';
 import TextSplitterComponent from '@/hooks/text/TextFill';
 import { useTheme } from '@/contexts/ThemeContext';
-
+import SubMenu from '../SubMenu';
 const StartProj = () => {
   const { theme, phoneScreenWidth, tabletScreenWidth, screenWidth, toggleTheme } = useTheme();
 
   return (
     <div className="startProj__container">
       {
-        tabletScreenWidth >=  screenWidth ? (
-          <div className="startProj__box__tablet">
-
-          </div>
+        tabletScreenWidth >= screenWidth ? (
+          <>
+            <div className="startProj__box__tablet">
+              <TextSplitterComponent tagType='h1' wordsPerLineArray={[3, 3, 2]} >
+                Stand Out Online Elevate Your Brand with Us
+              </TextSplitterComponent>
+              <Button className={`button_style_${theme}`} text="START YOUR PROJECT" />
+            </div>
+            <div className='startProj__subMenu'>
+              <SubMenu sectionNumber={0} isActive={0} />
+              <SubMenu dir='rtl' sectionNumber={1} isActive={1} />
+              <SubMenu sectionNumber={2} isActive={2} />
+            </div>
+            <div className="startProj__text">
+            <h1>SaaS Solutions Expert</h1>
+            <TextSplitterComponent tagType='p' wordsPerLineArray={[5, 5, 5,4,4,2]} >
+            As SaaS solution specialists, we collaborate with you to turn your vision into a thriving product, harnessing the power of cutting-edge technology for guaranteed success.
+            </TextSplitterComponent>
+            </div>
+          </>
         ) : (
           <>
             <Parallax className="startProj__left" scale={[0.7, 1]}>
--- app/(Home)/_components/SubMenu/index.jsx
+++ app/(Home)/_components/SubMenu/index.jsx
@@ -2,9 +2,9 @@ import { useState } from 'react'; // Changed double quotes to single
 import './index.styles.scss';     // Changed double quotes to single
 import { IconsSubMenu } from '@/data/IconsSubMenu'; // Changed double quotes to single
 
-const SubMenu = ({ isActive, sectionNumber }) => {
+const SubMenu = ({ isActive, sectionNumber ,dir='ltr' }) => {
     const renderItems = IconsSubMenu.map((item, index) => (
-      <div className='SubMenu-icon-container' key={index}>
+      <div dir={dir} className='SubMenu-icon-container' key={index}>
         <item.icon 
           className={`SubMenu-icon ${index === isActive ? 'active' : ''}`} 
           alt={item.title} 
