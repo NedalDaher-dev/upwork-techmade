591feaebd49964ce4ad99f2bec7f0016210b3155
AbdulRahmanPro <progammerabood@gmail.com>
2024-04-20 21:51:49 +0300

    save my work

--- app/(Home)/_components/Button/index.jsx
+++ app/(Home)/_components/Button/index.jsx
@@ -1,15 +1,20 @@
 import ArrowYellow from '@/assets/Icons/ArrowYellow';
 import React from 'react';
-import './index.style.scss';
 import useCalendlyPopup from '@/hooks/useCalendlyPopup';
+import Arrow from '@/assets/Icons/Arrow';
+import './index.style.scss';
 // Add a className prop to the Button component
 const Button = ({ text, className, onClick }) => {
-   const handleCalendlyPopup = useCalendlyPopup('https://calendly.com/umer-techmade/chat-with-umer-techmade');
-  
-   return (  
+  const handleCalendlyPopup = useCalendlyPopup('https://calendly.com/umer-techmade/chat-with-umer-techmade');
+
+  return (
     <div onClick={handleCalendlyPopup} className={` ${className} button__container`}> {/* Use the className prop */}
-      <button type="button">{text}</button>
-      <ArrowYellow className="arrow-icon-button" />
+      <button type="button">
+        {text}
+        <span className="arrow-icon-button">
+          <Arrow />
+        </span>
+      </button>
     </div>
   )
 }
