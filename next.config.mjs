9be079ea105d96b70d7b1d99b329b267daa4088d
AbdulRahmanPro <progammerabood@gmail.com>
2024-04-30 01:13:29 +0300

    useing postcss

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,17 @@
+import BrotliPlugin from 'brotli-webpack-plugin';
+
 export default {
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          { key: 'Cache-Control', value: 'no-store' }
-        ],
-      },
-    ]
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      config.plugins.push(
+        new BrotliPlugin({
+          asset: '[path].br[query]',
+          test: /\.(js|css|html|svg)$/,
+          threshold: 10240,
+          minRatio: 0.8
+        })
+      );
+    }
+    return config;
   },
-}
+};
